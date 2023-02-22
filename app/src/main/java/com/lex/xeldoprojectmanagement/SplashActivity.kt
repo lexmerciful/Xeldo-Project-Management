927d8b54710d6fc6138fc7e7278a6c14253077c3
Merciful Afolabi <lexmerciful@gmail.com>
2023-02-22 07:44:06 +0100

    Setting up splashscreen

---
+++ app/src/main/java/com/lex/xeldoprojectmanagement/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.lex.xeldoprojectmanagement
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
+import com.lex.xeldoprojectmanagement.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private var binding: ActivitySplashBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_splash)
+
+        // To hide the status bar and make the splash screen a full screen activity.
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "ethnocentric rg.otf")
+
+        val tvAppName: TextView = findViewById(R.id.tvAppName)
+        tvAppName.typeface = typeFace
+    }
+}
