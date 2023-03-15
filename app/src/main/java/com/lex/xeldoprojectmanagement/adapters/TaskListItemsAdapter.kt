2e0c77512555779d762b3425e6aecbbb78eaf2e8
Merciful Afolabi <lexmerciful@gmail.com>
2023-03-15 12:05:58 +0100

    Editing and deleting task list

--- app/src/main/java/com/lex/xeldoprojectmanagement/adapters/TaskListItemsAdapter.kt
+++ app/src/main/java/com/lex/xeldoprojectmanagement/adapters/TaskListItemsAdapter.kt
@@ -1,11 +1,13 @@
 package com.lex.xeldoprojectmanagement.adapters
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.lex.xeldoprojectmanagement.R
 import com.lex.xeldoprojectmanagement.activities.TaskListActivity
@@ -59,11 +61,69 @@ open class TaskListItemsAdapter(
                     }
                 }
             }
+
+            holder.binding.ibEditListName.setOnClickListener {
+                holder.binding.etEditTaskListName.setText(model.title)
+                holder.binding.llTitleView.visibility = View.GONE
+                holder.binding.cvEditTaskListName.visibility = View.VISIBLE
+            }
+
+            holder.binding.ibCloseEditableView.setOnClickListener {
+                holder.binding.llTitleView.visibility = View.VISIBLE
+                holder.binding.cvEditTaskListName.visibility = View.GONE
+            }
+
+            holder.binding.ibDoneEditListName.setOnClickListener {
+                val listName = holder.binding.etEditTaskListName.text.toString()
+
+                if (context is TaskListActivity){
+                    if (listName.isNotEmpty() && listName != model.title){
+                        context.updateTaskList(position,listName,model)
+                    } else if (listName == model.title){
+                        holder.binding.llTitleView.visibility = View.VISIBLE
+                        holder.binding.cvEditTaskListName.visibility = View.GONE
+                        Toast.makeText(context, "No changes made", Toast.LENGTH_SHORT).show()
+                    }else{
+                        context.showErrorSnackBar(context.getString(R.string.please_enter_list_name))
+                    }
+                }
+            }
+
+            holder.binding.ibDeleteList.setOnClickListener {
+                alertDialogForDeleteList(position, model.title)
+            }
         }
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    /**
+     * Function to show the Alert Dialog for deleting the task list.
+     */
+    private fun alertDialogForDeleteList(position: Int, title: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Delete Task List")
+        builder.setMessage("Are you sure you want to delete $title.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        //performing positive action
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+            dialogInterface.dismiss()
+
+            if (context is TaskListActivity) {
+                context.deleteTaskList(position)
+            }
+        }
+
+        //performing negative action
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 
     private fun Int.toDp(): Int = (this/ Resources.getSystem().displayMetrics.density).toInt()
