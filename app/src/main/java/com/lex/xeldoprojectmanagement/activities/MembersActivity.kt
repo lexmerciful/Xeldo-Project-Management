a2948acfd0c487bc89d20552f1963cd21adf0c21
Merciful Afolabi <lexmerciful@gmail.com>
2023-03-21 09:58:32 +0100

    Add new members dialog

--- app/src/main/java/com/lex/xeldoprojectmanagement/activities/MembersActivity.kt
+++ app/src/main/java/com/lex/xeldoprojectmanagement/activities/MembersActivity.kt
@@ -1,11 +1,15 @@
 package com.lex.xeldoprojectmanagement.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lex.xeldoprojectmanagement.R
 import com.lex.xeldoprojectmanagement.adapters.MembersListItemsAdapter
 import com.lex.xeldoprojectmanagement.databinding.ActivityMembersBinding
+import com.lex.xeldoprojectmanagement.databinding.DialogAddMemberBinding
 import com.lex.xeldoprojectmanagement.firebase.FirestoreClass
 import com.lex.xeldoprojectmanagement.models.Board
 import com.lex.xeldoprojectmanagement.models.Users
@@ -40,5 +44,40 @@ class MembersActivity : BaseActivity() {
 
         val adapter = MembersListItemsAdapter(this, list)
         binding.rvMembersList.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add_member ->{
+                dialogAddMember()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogAddMember(){
+        val dialogBinding: DialogAddMemberBinding = DialogAddMemberBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        dialog.setContentView(dialogBinding.root)
+        dialogBinding.tvAdd.setOnClickListener {
+            val email = dialogBinding.etEmailSearchMember.text.toString()
+
+            if (email.isNotEmpty()){
+                dialog.dismiss()
+
+            }else{
+                showErrorSnackBar(resources.getString(R.string.please_enter_member_email))
+            }
+        }
+        dialogBinding.tvCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
